30197e2094ba7cf8977b6e262a8286357f5a6a56
Oscar Vincent Andersen <eaaova@students.eaaa.dk>
2018-09-05 15:22:27 +0200

    test

--- src/opgave/Opgave.java
+++ src/opgave/Opgave.java
@@ -1,22 +1,19 @@
 package opgave;
 
 public class Opgave {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello!");
-		
+
 		System.out.println("her er ikke nok til alle");
 
+		System.out.println("dit nummer er:");
+		System.out.println(beregn(20, 18));
 
-        System.out.println(beregn(20,18));
-
-		
 	}
 
 	private static int beregn(int tal1, int tal2) {
-	    return tal1 * tal2 / 20;
-    }
-
-
+		return tal1 * tal2 / 20;
+	}
 
 }
